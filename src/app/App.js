52739921cf38c7819df8306f90cb7406da0b411e
Nripesh Rajbhandari <nripesh.rajbhandari@gmail.com>
2024-05-16 23:05:21 +0100

    features components Inventory.js Cart.js and CurrencyFilter.js Added and Imported to the top component App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,34 @@
-import './App.css';
+import { Inventory } from "../features/inventory/Inventory";
+import { Cart } from "../features/cart/Cart";
+import { CurrencyFilter } from "../features/currencyFilter/CurrencyFilter";
 
 function App(props) {
+
   const { state, dispatch } = props;
+
+  // Render the Cart component below <Inventory />
 
   return (
     <div>
-    
+      <CurrencyFilter
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
+      <Inventory
+        inventory={state.inventory}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
+      <Cart
+        cart = {state.cart}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
     </div>
   );
-}
+};
 
 export default App;
